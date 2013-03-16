5e7f4790e8bad6b62362d364a71a3247f01844bc
Phanx <addons@phanx.net>
2013-03-16 10:38:47 +0000

    - Make sure to wipe the cache when changing an option that affects the pet string display

--- Options.lua
+++ Options.lua
@@ -38,6 +38,7 @@ ShowCount:SetScript("OnClick", function(this)
 	local checked = not not this:GetChecked()
 	PlaySound(checked and "igMainMenuOptionCheckBoxOn" or "igMainmenuOptionCheckBoxOff")
 	BBPT_COUNT = checked
+	Addon.EventFrame:PET_JOURNAL_UPDATE()
 end)
 Options.ShowCount = ShowCount
 
@@ -48,6 +49,7 @@ ShowLevel:SetScript("OnClick", function(this)
 	local checked = not not this:GetChecked()
 	PlaySound(checked and "igMainMenuOptionCheckBoxOn" or "igMainmenuOptionCheckBoxOff")
 	BBPT_LEVEL = checked
+	Addon.EventFrame:PET_JOURNAL_UPDATE()
 end)
 Options.ShowLevel = ShowLevel
 
